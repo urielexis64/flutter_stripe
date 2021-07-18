393923ca1be681e5d04d96b6f805ec9276e303ef
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-18 17:00:39 -0600

    set main routes and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:stripe_app/pages/full_payment_page.dart';
+import 'package:stripe_app/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +9,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'StripeApp',
+      initialRoute: 'home',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'home': (_) => HomePage(),
+        'full_payment': (_) => FullPaymentPage(),
+      },
+      theme: ThemeData.light().copyWith(
+        primaryColor: const Color(0xFF284879),
+        scaffoldBackgroundColor: const Color(0xFF21232A),
+      ),
     );
   }
 }
