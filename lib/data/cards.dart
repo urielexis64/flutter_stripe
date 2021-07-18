bede3b7ec4b735dd6ccfa18588e1e51e66f12b24
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-18 17:19:30 -0600

    add credit cards data

---
+++ lib/data/cards.dart
@@ -0,0 +1,25 @@
+import 'package:stripe_app/models/custom_credit_card.dart';
+
+final List<CustomCreditCard> cards = <CustomCreditCard>[
+  CustomCreditCard(
+      cardNumberHidden: '4242',
+      cardNumber: '4242424242424242',
+      brand: 'visa',
+      cvv: '213',
+      expiracyDate: '01/25',
+      cardHolderName: 'Uriel Aispuro'),
+  CustomCreditCard(
+      cardNumberHidden: '5555',
+      cardNumber: '5555555555554444',
+      brand: 'mastercard',
+      cvv: '213',
+      expiracyDate: '01/25',
+      cardHolderName: 'Melissa Flores'),
+  CustomCreditCard(
+      cardNumberHidden: '3782',
+      cardNumber: '378282246310005',
+      brand: 'american express',
+      cvv: '2134',
+      expiracyDate: '01/25',
+      cardHolderName: 'Eduardo Rios'),
+];
