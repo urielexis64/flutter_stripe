b9c0b740cce32e30a1f50d8919cca51fab8456af
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-25 22:59:08 -0600

    finish pay with new card

--- lib/service/stripe_service.dart
+++ lib/service/stripe_service.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:stripe_app/models/payment_intent_response.dart';
 import 'package:stripe_app/models/stripe_response.dart';
 import 'package:stripe_payment/stripe_payment.dart';
 
@@ -9,10 +11,14 @@ class StripeService {
   factory StripeService() => _instance;
 
   String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
-  String _secretKey =
+  static String _secretKey =
       'sk_test_51JHJGtCEFy4tnxrG0MUM60FDdp7uVtuDhwPU6EGBFJIUywXIEWSKvwoTQUnKXcHvgtfZeYILmaZyfGwYhB6NkApz00z96v0Ef6';
   String _publishableKey =
       'pk_test_51JHJGtCEFy4tnxrGORFNdUusgDiV8ccja7ubHDvXQBhFA9HUIXrTnUMKuK6Q60aQIvi1gVEYxjMkVnuCAh1xq2zy00Tlwg6AYd';
+
+  final headerOptions = Options(
+      contentType: Headers.formUrlEncodedContentType,
+      headers: {'Authorization': 'Bearer $_secretKey'});
 
   void init() {
     StripePayment.setOptions(StripeOptions(
@@ -31,7 +37,11 @@ class StripeService {
     try {
       final paymentMethod = await StripePayment.paymentRequestWithCardForm(
           CardFormPaymentRequest());
-      return StripeResponse(ok: true);
+
+      final response = await _makePayment(
+          amount: amount, currency: currency, paymentMethod: paymentMethod);
+
+      return response;
     } catch (e) {
       return StripeResponse(ok: false, message: e.toString());
     }
@@ -40,11 +50,41 @@ class StripeService {
   Future payWithAppleOrGoogle(
       {required String amount, required String currency}) async {}
 
-  Future _createPaymentIntent(
-      {required String amount, required String currency}) async {}
+  Future<PaymentIntentResponse> _createPaymentIntent(
+      {required String amount, required String currency}) async {
+    try {
+      final dio = Dio();
+      final data = {'amount': amount, 'currency': currency};
+      final resp =
+          await dio.post(_paymentApiUrl, data: data, options: headerOptions);
 
-  Future _makePayment(
+      return PaymentIntentResponse.fromJson(resp.data);
+    } catch (e) {
+      print(e);
+      return PaymentIntentResponse(status: '400');
+    }
+  }
+
+  Future<StripeResponse> _makePayment(
       {required String amount,
       required String currency,
-      required PaymentMethod paymentMethod}) async {}
+      required PaymentMethod paymentMethod}) async {
+    try {
+      final paymentIntent =
+          await _createPaymentIntent(amount: amount, currency: currency);
+      final paymentResult = await StripePayment.confirmPaymentIntent(
+          PaymentIntent(
+              clientSecret: paymentIntent.clientSecret,
+              paymentMethodId: paymentMethod.id));
+
+      if (paymentResult.status == 'succeeded') {
+        return StripeResponse(ok: true);
+      }
+
+      return StripeResponse(
+          ok: false, message: 'Payment failed: ${paymentResult.status}');
+    } catch (e) {
+      return StripeResponse(ok: false, message: e.toString());
+    }
+  }
 }
