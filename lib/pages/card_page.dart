51a8ad2fe5b16e6b3ee2845a1de4bae3f04f1660
Uriel Aispuro <urielalexis64@gmail.com>
2021-07-25 18:54:17 -0600

    add willpopscope when back click

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:stripe_app/bloc/pay/pay_bloc.dart';
 import 'package:stripe_app/models/custom_credit_card.dart';
 import 'package:stripe_app/widgets/total_pay_button.dart';
 
@@ -11,29 +13,45 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pay'),
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: [
-          Positioned(
-            width: size.width,
-            height: size.height,
-            top: 0,
-            child: Hero(
-              tag: card.cardNumber,
-              child: CreditCardWidget(
-                  cardNumber: card.cardNumber,
-                  expiryDate: card.expiracyDate,
-                  cardHolderName: card.cardHolderName,
-                  cvvCode: card.cvv,
-                  showBackView: false),
+    final payBloc = context.watch<PayBloc>();
+
+    return WillPopScope(
+      onWillPop: () {
+        payBloc.add(OnDeactivateCreditCard());
+        Navigator.pop(context);
+        print('entro');
+        return Future.delayed(Duration(milliseconds: 10));
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Pay'),
+          leading: IconButton(
+              onPressed: () {
+                payBloc.add(OnDeactivateCreditCard());
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back)),
+          centerTitle: true,
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              width: size.width,
+              height: size.height,
+              top: 0,
+              child: Hero(
+                tag: card.cardNumber,
+                child: CreditCardWidget(
+                    cardNumber: card.cardNumber,
+                    expiryDate: card.expiracyDate,
+                    cardHolderName: card.cardHolderName,
+                    cvvCode: card.cvv,
+                    showBackView: false),
+              ),
             ),
-          ),
-          Positioned(bottom: 0, child: TotalPayButton())
-        ],
+            Positioned(bottom: 0, child: TotalPayButton())
+          ],
+        ),
       ),
     );
   }
